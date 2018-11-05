767fbb79edc1b063fedea8493456b2d998096f4d
trangsinh87 <trangsinh87@yahoo.com.vn>
2018-11-05 18:38:06 +0700

    update

--- js/visibility.js
+++ js/visibility.js
@@ -1,4 +1,12 @@
-var hidden, visibilityChange, isVisible = true;
+var hidden, visibilityChange, isActive = true,
+	handler = function(e) {
+		if (typeof e === 'boolean') {
+			isActive = e;
+		} else {
+			isActive = document.visibilityState === 'visible' || !document[hidden];
+		}
+		log('visibility', isActive + '<br/>', true);
+	};
 if (typeof document.hidden !== 'undefined') {
 	hidden = 'hidden';
 	visibilityChange = 'visibilitychange';
@@ -9,9 +17,21 @@ if (typeof document.hidden !== 'undefined') {
 	hidden = 'webkitHidden';
 	visibilityChange = 'webkitvisibilitychange';
 }
+
 if (typeof document.addEventListener !== 'undefined' && hidden) {
-	document.addEventListener(visibilityChange, function() {
-		isVisible = document.visibilityState === 'visible' || !document[hidden];
-		log('visibility', isVisible + '<br/>', true);
-	}, false);
+	document.addEventListener(visibilityChange, handler, false);
 }
+document.addEventListener('focus', function() {
+	handler(true);
+}, false);
+
+document.addEventListener('blur', function() {
+	handler(false);
+}, false);
+
+window.addEventListener('focus', function() {
+	handler(true);
+}, false);
+window.addEventListener('blur', function() {
+	handler(false);
+}, false);
